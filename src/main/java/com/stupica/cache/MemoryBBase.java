6069dc2be13c6754ebbf1e352be491d9447b3c17
Bostjan Stupica <bostjan@stupica.com>
2021-12-09 22:44:40 +0100

    Add/Update: period constants.

--- src/main/java/com/stupica/cache/MemoryBBase.java
+++ src/main/java/com/stupica/cache/MemoryBBase.java
@@ -3,9 +3,11 @@ package com.stupica.cache;
 
 public class MemoryBBase {
 
-    public final int      nPERIOD_05min_InMILLIS = 1000 * 60 * 05;
-    public final int      nPERIOD_10min_InMILLIS = 1000 * 60 * 10;
-    public final int      nPERIOD_20min_InMILLIS = 1000 * 60 * 20;
+    public static final int      nPERIOD_01min_InMILLIS = 1000 * 60 * 01;
+    public static final int      nPERIOD_05min_InMILLIS = 1000 * 60 * 05;
+    public static final int      nPERIOD_10min_InMILLIS = 1000 * 60 * 10;
+    public static final int      nPERIOD_20min_InMILLIS = 1000 * 60 * 20;
+    public static final int      nPERIOD_30min_InMILLIS = 1000 * 60 * 30;
 
     protected final int   nMAX_COUNT_ELEMENT_DEF = 10000;
     protected final int   nPERIOD_RETENTION_SEC_DEF = 60 * 10;    // = 10 min;
